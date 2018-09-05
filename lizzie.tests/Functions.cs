ccf13b3422dd113e99e8f367edce7c5eb53746ad
Thomas Hansen <phosphorusfive@gmail.com>
2018-09-05 07:30:16 +0300

    Adding more unit tests

--- lizzie.tests/Functions.cs
+++ lizzie.tests/Functions.cs
@@ -109,5 +109,80 @@ bar");
             }
             Assert.AreEqual(true, success);
         }
+
+        [Test]
+        public void NestedFunctions()
+        {
+            var lambda = LambdaCompiler.Compile(@"
+var(@func1, function({
+  var(@func2, function({
+    +(bar2, '_yup')
+  }, @bar2))
+  func2(bar1)
+}, @bar1))
+
+func1('success')
+");
+            var result = lambda();
+            Assert.AreEqual("success_yup", result);
+        }
+
+        [Test]
+        public void NestedFunctionsThrows()
+        {
+            var lambda = LambdaCompiler.Compile(@"
+var(@func1, function({
+  var(@func2, function({
+    bar2
+  }, @bar2))
+  func2(bar1)
+}, @bar1))
+
+func2('success')
+");
+            var success = false;
+            try {
+                lambda();
+            } catch(LizzieRuntimeException) {
+                success = true;
+            }
+            Assert.AreEqual(true, success);
+        }
+
+        [Test]
+        public void TwoFunctions()
+        {
+            var lambda = LambdaCompiler.Compile(@"
+var(@func1, function({
+  +('success1_',bar1)
+}, @bar1))
+var(@func2, function({
+  +('_success2_',bar2)
+}, @bar2))
++(func1('foo'), func2('bar'))
+");
+            var result = lambda();
+            Assert.AreEqual("success1_foo_success2_bar", result);
+        }
+
+        [Test]
+        public void NoCode()
+        {
+            var lambda = LambdaCompiler.Compile("");
+            var result = lambda();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void EmptyFunction()
+        {
+            var lambda = LambdaCompiler.Compile(@"
+var(@func, function({
+}))
+func()
+");
+            var result = lambda();
+            Assert.IsNull(result);
+        }
     }
 }
